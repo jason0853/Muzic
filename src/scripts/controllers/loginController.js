7fcfb29f38ae895473fc5027df50d66ff164b673
이재성 <jason0853@gmail.com>
2016-06-14 00:41:24 +0900

    Append flipInY animation to login page.

--- src/scripts/controllers/loginController.js
+++ src/scripts/controllers/loginController.js
@@ -1,10 +1,12 @@
 module.exports = function($scope) {
 
+    // initial singup flag value
     $scope.signupStatus = false;
 
     // sign up button toggle event
     $scope.signupToggle = function() {
         $scope.signupStatus = !$scope.signupStatus;
-        console.log($scope.signupStatus);
     };
+
+    
 };
