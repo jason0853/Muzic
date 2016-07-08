45d775dd331363cef1313a523e74006322ecf0e8
이재성 <jason0853@gmail.com>
2016-07-09 03:27:51 +0900

    active status of buttons and markup design in list container

--- src/scripts/controllers/mainController.js
+++ src/scripts/controllers/mainController.js
@@ -3,25 +3,28 @@ module.exports = function($scope, toastr, youtubeFactory) {
     // Insert data
     $scope.youtubeData = youtubeFactory;
 
+    // Define an object property to access scope
+    $scope.text = { keyword: ''};
+
     // Load data
     youtubeFactory.init();
 
-    // Define an object property to access scope
-    $scope.text = { keyword: ''};
+    $scope.isActive = true;
 
     // Load recent data
     $scope.loadRecentMuzic = function() {
         youtubeFactory.init();
+        $scope.isActive = true;
     };
 
     // Load pupluar data
     $scope.loadPopularMuzic = function() {
         youtubeFactory.popularData();
+        $scope.isActive = false;
     };
 
     // Search music keyword
     $scope.loadSearchMuzic = function(keyword) {
-
         if (keyword == '') {
             toastr.warning('Please enter some keywords!', 'Warning', {
                 closeButton: true
